a26975e51855950a4dff64197053aad7a174c13c
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-05-13 12:12:42 -0300

    Add files via upload

---
+++ Calculadora.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1> Calucladora </h1>";
+$a = 10;
+$b = 5;
+$soma = $a + $b;
+echo "<h2>  Soma </h2>";
+echo "$a + $Sb = $soma";
+
+$c = 5;
+$d = 3;
+$subtracao = $c -$d;
+echo "<h2> Subtração </h2>";
+echo "$c - $d = $subtracao";
+
+$e = 8;
+$f = 2;
+$multiplicacao = $e* $f;
+echo "<h2> Multiplicação </h2>";
+echo "$e * $f = $multiplicacao";
+
+$g = 8;
+$h = 4;
+$divisao = $g / $h;
+echo "<h2> Divisão </h2>";
+echo "$g/ $h = $subtracao";
+?> 
+
+</body>
+</html>
