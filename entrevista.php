2ab1cdc30c9f6bc95741195aac610f2c9d299320
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-05-14 10:10:34 -0300

    Add files via upload

---
+++ entrevista.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1> Projeto Integrador </h1>";
+$nome = "Afonso";
+$idade = "16";
+echo "<h2> Entrevista </h2>";
+echo "<br>";
+echo "<h3> Olá, $nome. Como vai? </h3>";
+echo "<p> Oi, estou bem. </p>";
+echo "<h3> Eu gostaria de perguntar algumas coisas sobre você. Vai ser uma entrevista. </h3>";
+echo "<p> Pergunte. </p>";
+echo "<h3> Qual o seu nome completo? </h3>";
+echo "<p> Meu nome é $nome Ribeiro Coelho. </p>";
+echo "<h3> Quantos anos você tem? </h3>";
+echo "<p> $idade anos. </p>";
+echo "<h3> Ouvi dizer que você tem TDAH, é verdade? </h3>";
+echo "<p> Sim, fui diagnosticado aos 3 anos. </p>";
+echo "<h3> Ok, me falaram que você estuda no SENAC, o que você acha da escola? </h3>";
+echo "<p> Eu acho o SENAC uma escola bem legal, eu faço as atividades, eu gosto dos professores e consigo entender as aulas! </p>";
+echo "<h3> E como você tem TDAH, você também faz ativdades no AEE? </h3>";
+echo "<p> Sim, eu faço atividades com uma professora bem querida, ela se chama Tatiana! </p>";
+echo "<h3> E como você se sente no Ensino Médio do SENAC? </h3>";
+echo "<p> Estou no 2º Ano do Ensino Médio, e eu sinto que o ano passou bem rápido! Pois dois anos atrás, eu estava no 9º Ano do Ensino Fundamental, e no ano que vem, já estarei no 3º Ano do Médio! E depois disso, eu estarei e formando! É impressionante! </p>";
+?> 
+
+</body>
+</html>
