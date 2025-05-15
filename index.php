c7248b551f8e06cc7835cf6c3eb3c16e202463aa
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-05-15 12:13:02 -0300

    Add files via upload
    Beatriz,Lucca e Rafa

--- index.php
+++ index.php
@@ -5,34 +5,12 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="windows-1252">
         <title></title>
     </head>
-    <style>
-        h1 {
-          font-family:arial;
-          font-size: 30px;
-          text-align: center;
-          color: blue;
-            
-        }
-        p {
-          font-family:arial;
-          font-size: 20px;
-          text-align: left;
-          color: yellow;
-            
-        }
-    </style>
     <body>
         <?php
-        echo "<h1>Primeiro Programa em PHP</h1>";
-        echo "<br>";
-        echo "<p>Olá Mundo!</p>";
-        $nome = "Barry Allen";
-        echo "<br>";
-        echo "<p>Bom dia $nome!</p>";
-        
+        // put your code here
         ?>
     </body>
 </html>
