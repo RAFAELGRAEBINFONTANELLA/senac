43383174a916d2f1f3f358a7497e5bfbb7be144c
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-05-15 12:11:19 -0300

    Add files via upload
    Beatriz, Lucca e Rafa

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <style>
+        h1 {
+          font-family:arial;
+          font-size: 30px;
+          text-align: center;
+          color: blue;
+            
+        }
+        p {
+          font-family:arial;
+          font-size: 20px;
+          text-align: left;
+          color: yellow;
+            
+        }
+    </style>
+    <body>
+        <?php
+        echo "<h1>Primeiro Programa em PHP</h1>";
+        echo "<br>";
+        echo "<p>Olá Mundo!</p>";
+        $nome = "Barry Allen";
+        echo "<br>";
+        echo "<p>Bom dia $nome!</p>";
+        
+        ?>
+    </body>
+</html>
