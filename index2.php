984d28f3437926b9479d0f4df9090c31885cbfea
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-05-15 12:16:17 -0300

    Add files via upload
    Beatriz,Lucca e Rafa

---
+++ index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //variaveis
+        echo "<h1>PDV</h1>";
+        $codigo=107;
+        $nome = "Limpa tênis" ;
+        $qtde = 183;
+        $valor = 15.00;
+        echo "<h1>PVD</h1>";
+        //calculo
+        $valrtotal = $valor * $qtde;
+        //escreva na tela
+        echo "Valor Total é igual a $vlrtotal";
+        
+        ?>
+    </body>
+</html>
