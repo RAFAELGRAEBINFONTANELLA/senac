9b5454b2c4b67d2e8c9e04772903cee3b2351ad8
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-05-15 12:14:48 -0300

    Add files via upload
    Beatriz Lucca e Rafa

---
+++ vendas.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
