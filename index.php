e167a0d0a7fb58dfa2eb9878e6f7d9e65a2382a9
RAFAELGRAEBINFONTANELLA <fontanella.rafa18@gmail.com>
2025-06-10 12:18:43 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,47 +6,19 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> Escola de Música </title>
-        <link rel="stylesheet" href="estilo.css"
+        <title>Loja Dia dos Namorados</title>
     </head>
     <body>
-        <form method="post">
-            <h1> Cadastro do Aluno </h1>
-            <label for="fcod">Código:</label><br>
-            <input type="text" id="codigo" name="codigo" value=""><br>
-            <label for="fname">Nome:</label><br>
-            <input type="text" id="nome" name="nome" value=""><br>
-            <label for="femail">E-mail:</label><br>
-            <input type="text" id="email" name="email" value=""><br>
-            <label for="ffone">Fone:</label><br>
-            <input type="text" id="fone" name="fone" value=""><br>
-            <input type="submit" value="Cadastrar">
-        </form>
         <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST"){
-           $codigo = $_POST["codigo"];
-           $nome = $_POST["nome"];
-           $email = $_POST["email"];
-           $fone = $_POST["fone"];
-           echo "<h1> Dados do Alunos </h1>";
-           echo "Nome:  $nome <br>";
-           echo "E-mail: $email <br>";
-           echo "Fone: $fone <br>";
-        // Conectar o banco de dados
-        $caminho_banco = 'bdmusica.db';
-        $pdo = new PDO ("sqlite: " . $caminho_banco);
-        $pdo->setAttribute (PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
-        echo "Conexão com o banco de dados SQLite realizada "
-        . "com sucesso!<br>" ;
-        //
-        $sql_inserir_dados = "
-        INSERT INTO alunos (id_aluno, nome, email, fone, ativo)
-        VALUES ($codigo, $nome, $email, $fone, 'A'); 
-        ";
-        $pdo->exec ($sql_inserir);
-        echo "Aluno cadastrado com sucesso!";
-       
-        }
+        //Conectar o banco de dados
+        $caminho_banco = 'bdloja.db';
+        $pdo = new PDO("sqlite:" . $caminho_banco);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo "Conexão com o banco de dados SQLite realizada com sucesso!<br>";
+        // Listar todos os produtos
+        $sql = "SELECT * FROM produtos ORDER by nome";
+        $stmt =$pdo->query($sql); //prepara a consulta
+        //Retorna todos os alunos
         ?>
     </body>
 </html>
